141be053a3aca464b8e87c370ad472ab4c62c765
LiZhiyang <615945987a7@gmail.com>
2018-08-12 11:51:06 +0800

    Update 02 to function02!

--- src/test01.java
+++ src/test01.java
@@ -11,3 +11,8 @@ Test thoes commands of branch management！
 
 This is the first chages for second branch of function02!
 
+
+
+
+Update 02 to functin02
+This is the cahnges for second branch of function02!
